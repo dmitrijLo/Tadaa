2b605b638127234ebabd37916032344935d35b2b
Shawn GB <god.shawn@gmail.com>
2026-01-12 15:50:34 +0100

    feat(guests): add findOneByToken method to GuestsService

--- apps/backend-nestjs/src/guests/guests.service.ts
+++ apps/backend-nestjs/src/guests/guests.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGuestDto } from './dto/create-guest.dto';
 import { UpdateGuestDto } from './dto/update-guest.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Guest } from './entities/guest.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class GuestsService {
+  constructor(
+    @InjectRepository(Guest)
+    private guestRepository: Repository<Guest>,
+  ) {}
+
+  // get guestpage by token
+  findOneByToken(token: string) {
+    const guest = this.guestRepository.findOne({
+      where: { inviteToken: token },
+    });
+
+    return guest;
+  }
+
+  // default generate resources
   create(createGuestDto: CreateGuestDto) {
     return 'This action adds a new guest';
   }
