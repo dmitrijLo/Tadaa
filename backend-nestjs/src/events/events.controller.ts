efb95e73aa9bb1c88dcef57f00f08de7e564a647
dmitrijLo <demilorenz@googlemail.com>
2026-01-14 17:56:58 +0100

    refactor create -> createEvent, add swagger decorater, change generic Error -> UnauthorizedError

--- backend-nestjs/src/events/events.controller.ts
+++ backend-nestjs/src/events/events.controller.ts
@@ -8,27 +8,45 @@ import {
   Delete,
   UseGuards,
   Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { JwtDevGuard } from '../auth/guards/jwt-dev.guard';
 import type { Request } from 'express';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 interface RequestWithUser extends Request {
   user?: { id: number };
 }
 
 @Controller('events')
+@ApiTags('Events')
+@ApiBearerAuth()
 @UseGuards(JwtDevGuard)
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  create(@Body() createEventDto: CreateEventDto, @Req() req: RequestWithUser) {
+  @ApiOperation({ summary: 'Create a new event.' })
+  @ApiCreatedResponse({ description: 'Event has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Validation failed.' })
+  @ApiUnauthorizedResponse({ description: 'User not logged in.' })
+  createEvent(
+    @Body() createEventDto: CreateEventDto,
+    @Req() req: RequestWithUser,
+  ) {
     const hostId = req.user?.id;
     if (!hostId) {
-      throw new Error('User ID is required');
+      throw new UnauthorizedException('User ID is required');
     }
     return this.eventsService.create(createEventDto, hostId.toString());
   }
