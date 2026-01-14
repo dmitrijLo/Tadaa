fe08d5ff9dc57116b26181c110ee2686b5bd34c8
dmitrijLo <demilorenz@googlemail.com>
2026-01-14 17:32:00 +0100

    implement Guests Component on dashboard for testing

--- frontend-nextjs/app/dashboard/page.tsx
+++ frontend-nextjs/app/dashboard/page.tsx
@@ -1,8 +1,21 @@
-export default function DashboardPage() {
+import GuestList from "@/components/guest/GuestList";
+import { TEST_EVENT_UUID, TEST_ROUTE } from "@/utils/api";
+
+export default async function DashboardPage() {
+  const response = await fetch(TEST_ROUTE, {
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+      "x-dev-user-id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
+    },
+  });
+
+  if (!response.ok) return <div>Fehler beim Laden der Gäste</div>;
+
+  const initialGuests = await response.json();
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Hier entsteht bald Großes.</p>
+    <div className="p-8">
+      <GuestList eventId={TEST_EVENT_UUID} initialGuests={initialGuests} />
     </div>
   );
 }
