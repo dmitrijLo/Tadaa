5a4ded5882d13c6c75167f49bb3fa99c11f3c11b
dmitrijLo <demilorenz@googlemail.com>
2026-01-15 13:49:29 +0100

    refactor api utils method. it now works for client & server components, make use of GuestComponent in DashboardPage

--- frontend-nextjs/app/dashboard/page.tsx
+++ frontend-nextjs/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
 import GuestList from "@/components/guest/GuestList";
-import { TEST_EVENT_UUID, TEST_ROUTE } from "@/utils/api";
+import { BACKEND_URL, getAuthHeader, TEST_EVENT_UUID } from "@/utils/api";
 
 export default async function DashboardPage() {
-  const response = await fetch(TEST_ROUTE, {
-    cache: "no-store",
-    headers: {
-      "Content-Type": "application/json",
-      "x-dev-user-id": "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
+  const response = await fetch(
+    `${BACKEND_URL}/events/${TEST_EVENT_UUID}/guests`,
+    {
+      cache: "no-store",
+      headers: getAuthHeader(),
     },
-  });
+  );
 
   if (!response.ok) return <div>Fehler beim Laden der Gäste</div>;
 
--- frontend-nextjs/utils/api.ts
+++ frontend-nextjs/utils/api.ts
@@ -1,19 +1,24 @@
 import axios from "axios";
 
-export const TEST_EVENT_UUID = "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11";
-export const TEST_ROUTE = `http://localhost:3000/events/${TEST_EVENT_UUID}/guests`;
+const isClient = typeof window !== "undefined";
+
+export const TEST_EVENT_UUID = "e77b9a3f-911d-41d4-807b-8f4e315c6f31";
+export const TEST_EVENTHOST_UUID = "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11";
+export const TEST_ROUTE = `http://localhost:3001/events/${TEST_EVENT_UUID}/guests`;
+
+export const BACKEND_URL = isClient
+  ? process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001"
+  : process.env.INTERNAL_API_URL || "http://backend:3001";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: BACKEND_URL,
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use((cfg) => {
   const isDev = process.env.NODE_ENV === "development";
   if (isDev) {
-    cfg.headers["x-dev-user-id"] = TEST_EVENT_UUID;
+    cfg.headers["x-dev-user-id"] = TEST_EVENTHOST_UUID;
   } else {
     //token
   }
@@ -23,3 +28,15 @@ api.interceptors.request.use((cfg) => {
 
   return cfg;
 });
+
+export const getAuthHeader = () => {
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    headers["x-dev-user-id"] = TEST_EVENTHOST_UUID;
+  }
+
+  return headers;
+};
