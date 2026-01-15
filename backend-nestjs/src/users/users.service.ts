1f2f69222f6048f4688c74ecc308438942bbea85
dmitrijLo <demilorenz@googlemail.com>
2026-01-15 14:03:58 +0100

    add seeding with initial dev-user & dev-event

--- backend-nestjs/src/users/users.service.ts
+++ backend-nestjs/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -7,10 +7,37 @@ import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {
-  constructor(
-    @InjectRepository(User) private usersRepository: Repository<User>,
-  ) {}
+export class UsersService implements OnApplicationBootstrap {
+  private readonly logger = new Logger(UsersService.name);
+  constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+  async onApplicationBootstrap() {
+    await this.seedDevUser();
+  }
+
+  private async seedDevUser() {
+    if (process.env.JWT_DEV_MODE !== 'true') {
+      return;
+    }
+
+    const devUserId = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11';
+    const userExists = await this.usersRepository.findOneBy({ id: devUserId });
+
+    if (!userExists) {
+      this.logger.log('Seeding default Dev-User');
+      const devUser = this.usersRepository.create({
+        id: devUserId,
+        email: 'dev@tadaa.app',
+        name: 'max_power',
+        passwordHash: await bcrypt.hash('super-secret', 12),
+      });
+
+      await this.usersRepository.save(devUser);
+      this.logger.log(`Dev-User created: ${devUserId}`);
+    } else {
+      this.logger.debug('Dev-User already exists. Skipping seed.');
+    }
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const user = this.usersRepository.create(createUserDto);
