aa72b2bd5b39bfd5c79f9c3c27392659e4c20f42
dmitrijLo <demilorenz@googlemail.com>
2026-01-22 10:30:46 +0100

    add mail module and basic template

---
+++ backend-nestjs/src/mail/mail.service.ts
@@ -0,0 +1,29 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Guest } from 'src/guests/entities/guest.entity';
+
+@Injectable()
+export class MailService {
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async sendGuestInvitation(guest: Guest, eventName: string) {
+    const baseUrl = this.configService.get('FRONTEND_URL') || 'http://localhost:3000';
+    const { id: guestId, email, name } = guest;
+    const invitationLink = `${baseUrl}/invitation/${guestId}`;
+
+    return this.mailerService.sendMail({
+      to: email,
+      subject: `Einladung zu ${eventName}`,
+      template: './invitation',
+      context: {
+        name,
+        eventName,
+        link: invitationLink,
+      },
+    });
+  }
+}
