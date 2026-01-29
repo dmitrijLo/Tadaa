58e8d998aa66bb40226db491762f43eab48b042b
dmitrijLo <demilorenz@googlemail.com>
2026-01-29 17:41:42 +0100

    refactor: simplify the api-call in ReadinessComponente

--- frontend-nextjs/components/event/ReadinessComponent.tsx
+++ frontend-nextjs/components/event/ReadinessComponent.tsx
@@ -2,25 +2,14 @@ import { notFound } from "next/navigation";
 import GuestListing from "./readiness/GuestlistComponent";
 import { makeApiRequest, BACKEND_URL } from "@/utils/api";
 
-const fetchEventUsers = async (eventid: string) => {
-  const { data, error } = await makeApiRequest<Guest[]>(
-    `${BACKEND_URL}/events/${eventid}/readiness`,
-  );
-
-  if (error) {
-    console.error(error);
-    return undefined;
-  }
-
-  return data;
-};
-
 export default async function ReadinessComponent({
   eventid,
 }: {
   eventid: string;
 }) {
-  const users = await fetchEventUsers(eventid);
+  const { data: users, error } = await makeApiRequest<Guest[]>(
+    `${BACKEND_URL}/events/${eventid}/readiness`,
+  );
   if (!users) notFound();
 
   return (
